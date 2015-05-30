92a01c1665ebd82518604034dc2f03cb537f18d6
Jan Bölsche <regular.gonzales@googlemail.com>
2015-05-30 15:04:54 +0200

    using gh-pages module instead of homegrown implementation

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
-// npm node modules
-var shellby = require('shellby');
 var git = require('parse-git-config');
+var ghpages = require('gh-pages');
 
-
-exports.publish = function(config, opts, getEpisodeUrl) {
+exports.publish = function(config, opts, cb) {
+    console.log('new bundler');
     var gitConfig = git.sync();
     if (gitConfig === null) {
-        console.error('This is not a git repository. Create one first and set a remote. The bpm bundle will be published on the gh-pages branch of this repository.');
-        process.exit(1);
+        return cb(new Error('This is not a git repository. Create one first and set a remote. The bpm bundle will be published on the gh-pages branch of this repository.'));
     }
-    var repoUrl = gitConfig['remote "origin"'].url;
-    var commands = [
-        'rm -rf .git',
-        'git init',
-        'git remote add pages ' + repoUrl,
-        'git checkout --orphan gh-pages',
-        'git add index.js',
-        'git commit -am "publish"',
-        'git push pages gh-pages --force'
-    ];
-
-    process.chdir('.bpm');
-    shellby.series(commands, function(err) {
-        if (err) throw err;
-        var result = repoUrl.match(/.*:(.*)\/(.*)\.git$/);
-        var org = result[1];
-        var repoName = result[2];
-        console.log('done publishing to ' + getEpisodeUrl(org, repoName));
-    });
+    ghpages.publish('.bpm', {
+        /* tag: */
+        logger: function(m) {
+            console.log(m);
+        },
+        message: 'auto publish',
+        clone: '.bpm/.clone'
+    }, cb);
 };
