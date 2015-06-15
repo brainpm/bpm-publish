4342522f321f11e4ef8fa3f90c48b92a2aff0078
Jan Bölsche <regular.gonzales@googlemail.com>
2015-06-15 17:41:40 +0200

    Add missing repo option to gh-pages.publish() call fixes `bpm rebundle-all`. Make sure to use relative paths when calling publish() to work around some bugs in gh-pages module

--- index.js
+++ index.js
@@ -9,16 +9,25 @@ exports.publish = function(config, opts, repoDir, bundleDir, cb) {
         return cb(new Error('This is not a git repository. Create one first and set a remote. The bpm bundle will be published on the gh-pages branch of this repository.'));
     }
     debug('found local git repo');
+    var repoUrl = gitConfig['remote "origin"'].url;
+    debug('remote "origin" is at %s', repoUrl);
+
+    var deployDir = path.join(repoDir, '.bpm', 'deploy')
+
+    // gh-pages seems to have trouble with abosulte paths
+    bundleDir = path.relative(process.cwd(), bundleDir);
+    deployDir = path.relative(process.cwd(), deployDir);
+
     ghpages.publish(bundleDir, {
         /* tag: */
+        repo: repoUrl,
         logger: function(m) {
             debug(m);
         },
         message: 'auto publish',
-        clone: path.join(bundleDir, '.clone')
+        clone: deployDir
     }, function(err) {
         if (err) return cb(err);
-        var repoUrl = gitConfig['remote "origin"'].url;
         var result = repoUrl.match(/.*:(.*)\/(.*)\.git$/);
         var org = result[1];
         var repoName = result[2];
