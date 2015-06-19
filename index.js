0a2490628a79c5176f4d3b9ae87c217ae1a74e59
Jan Bölsche <regular.gonzales@googlemail.com>
2015-06-19 15:43:24 +0200

    Fix github url parsing by replacing regex with module github-url

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var path = require('path');
 var git = require('parse-git-config');
 var ghpages = require('gh-pages');
+var parseGithubUrl = require('github-url');
 var debug = require('debug')('bpm-publish');
 
+function ghpagesPublish(bundleDir, options, cb) {
+    ghpages.publish(bundleDir, options, function(err) {
+        try {
+            cb(err);
+        } catch(e) {
+            console.error(e.stack);
+            process.exit(1);
+        }
+    });
+}
+
 exports.publish = function(config, opts, repoDir, bundleDir, cb) {
+    debug(require('./package.json').version);
     var gitConfig = git.sync({path: path.join(repoDir,'.git/config')});
     if (gitConfig === null) {
         return cb(new Error('This is not a git repository. Create one first and set a remote. The bpm bundle will be published on the gh-pages branch of this repository.'));
@@ -17,8 +30,9 @@ exports.publish = function(config, opts, repoDir, bundleDir, cb) {
     // gh-pages seems to have trouble with abosulte paths
     bundleDir = path.relative(process.cwd(), bundleDir);
     deployDir = path.relative(process.cwd(), deployDir);
+    debug('calling ghpages.publish');
 
-    ghpages.publish(bundleDir, {
+    ghpagesPublish(bundleDir, {
         /* tag: */
         repo: repoUrl,
         logger: function(m) {
@@ -27,14 +41,15 @@ exports.publish = function(config, opts, repoDir, bundleDir, cb) {
         message: 'auto publish',
         clone: deployDir
     }, function(err) {
+        debug('ghpages.publish done with error ' + err);
         if (err) return cb(err);
-        var result = repoUrl.match(/.*:\/+(.*)\/(.*)\.git$/);
-        var org = result[1];
-        var repoName = result[2];
+        debug('repoUrl', repoUrl);
+        var components = parseGithubUrl(repoUrl);
+        debug('url parse result', JSON.stringify(components));
         cb(null, {
             repositoryUrl: repoUrl,
-            organisation: org,
-            repositoryName: repoName
+            organisation: components.user,
+            repositoryName: components.project
         });
     });
 };
