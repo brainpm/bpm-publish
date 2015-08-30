5aa84e7418aca48780e5755dd83f0ce82276d673
Jan Bölsche <regular.gonzales@googlemail.com>
2015-08-30 16:40:30 +0200

    Add integration test for getRetrievalStream().

--- integration-test.js
+++ integration-test.js
@@ -1,11 +1,25 @@
+var test = require('tape');
 var getRetrievalStream = require('./lib/get-retrieval-stream');
 var through = require('through');
 var concat = require('concat-stream');
-getRetrievalStream('shecodes-content', 'foo').pipe(through(function(episode) {
-    console.log(episode.name);
-    if (episode.name === 'intro') {
-        episode.getFileStream('package.json').pipe(concat(function(json) {
-            console.log('JSON', json.toString('utf8'));
+var jsonStream = require('JSONStream');
+
+test('getRetrievalStream gets array of repo names and functions', function(t) {
+    getRetrievalStream('brainpm-testcases', 'brainpm-testcases').pipe(concat(function(receivedArray) {
+        t.equal(receivedArray.length, 1);
+        var first = receivedArray[0];
+        t.equal(first.name, 'teach-nothing');
+        t.equal(typeof first.getFileStream, 'function');
+        t.end();
+    }));
+});
+
+test('getFileStream("package.json") returns stream of file content', function(t) {
+    getRetrievalStream('brainpm-testcases', 'brainpm-testcases').pipe(concat(function(receivedArray) {
+        var first = receivedArray[0];
+        first.getFileStream("package.json").pipe(jsonStream.parse()).pipe(through(function(pkg) {
+            t.equal(pkg.name, 'teach-nothing');
+            t.end();
         }));
-    }
-}));
+    }));
+});
